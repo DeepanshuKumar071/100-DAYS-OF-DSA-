e644429bf3a7b898da99e7467eb099718cf2a3af
DeepanshuKumar071 <deepanshukr704@gmail.com>
2026-05-01 08:42:59 +0530

    DAY-19

---
+++ Q19.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    if (n <= 0)
+    {
+        printf("Invalid input");
+        return 0;
+    }
+
+    int arr[n];
+
+    // Input elements
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    // Find max and min
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+            max = arr[i];
+        if (arr[i] < min)
+            min = arr[i];
+    }
+
+    // Output
+    printf("Max: %d\n", max);
+    printf("Min: %d\n", min);
+
+    return 0;
+}
