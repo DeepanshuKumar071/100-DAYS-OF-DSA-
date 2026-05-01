f807afe76c06520965b7545908313ba2a052244c
DeepanshuKumar071 <deepanshukr704@gmail.com>
2026-05-01 08:43:54 +0530

    DAY-21

---
+++ Q21.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define node structure
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *temp = NULL, *newNode;
+
+    for (int i = 0; i < n; i++)
+    {
+        int value;
+        scanf("%d", &value);
+
+        // Create new node
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        // First node
+        if (head == NULL)
+        {
+            head = newNode;
+            temp = newNode;
+        }
+        else
+        {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Traverse and print
+    temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
